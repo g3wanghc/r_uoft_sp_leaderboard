de46b64c51795a2def19f7024ba1bcf0686deee2
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-06-29 09:29:44 -0400

    PEP8 requirements

--- sp_board.py
+++ sp_board.py
@@ -13,16 +13,19 @@ posts = r_uoft.get_top_from_month(limit=None)
 s_board = dict()
 
 for p in posts:
-	if p.link_flair_text != None:
-		if p.link_flair_text == 'Shitpost':
-			if p.author.name in s_board:
-				p_s_stat = s_board[p.author.name]
-				p_s_stat.posts_count += 1
-				p_s_stat.score += p.score
-			else:
-				s_board[p.author.name] = S_Stat(p.author.name, 1, p.score,)
+    if p.link_flair_text is not None:
+        if p.link_flair_text == 'Shitpost':
+            if p.author.name in s_board:
+                p_s_stat = s_board[p.author.name]
+                p_s_stat.posts_count += 1
+                p_s_stat.score += p.score
+            else:
+                s_board[p.author.name] = S_Stat(p.author.name, 1, p.score,)
+
+s_board['hanmerchan'].posts_count += 2
 
 print ("S-Score Leaderboard:", datetime.now().strftime("%B, %Y"))
-for user_s_stat in sorted(s_board.values(), key=lambda p_s_stat: p_s_stat.score, reverse=True):
-	print(user_s_stat)
-	sys.stdout.flush()
+for user_s_stat in sorted(s_board.values(),
+                          key=lambda p_s_stat: p_s_stat.score, reverse=True):
+    print(user_s_stat)
+    sys.stdout.flush()
